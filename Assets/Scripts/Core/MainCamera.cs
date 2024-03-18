53d2bcb2dc3f29d949c297c3919d066a29145f96
Siem15 <90563650+SiemWesseling@users.noreply.github.com>
2024-03-18 03:03:10 +0100

    added some comments

--- Assets/Scripts/Core/MainCamera.cs
+++ Assets/Scripts/Core/MainCamera.cs
@@ -4,10 +4,13 @@ public class MainCamera : MonoBehaviour {
 
     Portal[] portals;
 
+    //Find all portals in scene
     void Awake () {
         portals = FindObjectsOfType<Portal> ();
     }
 
+    //Before the camera starts the culling operation, 
+    //check the pre-, post- and normal rendering functions of each of the portals in the scene
     void OnPreCull () {
 
         for (int i = 0; i < portals.Length; i++) {
